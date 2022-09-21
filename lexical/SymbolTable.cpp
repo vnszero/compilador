23d588695d6709d66189761634f5f6db6646adc4
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-09-20 22:03:15 -0300

    Create Symbol Table

---
+++ lexical/SymbolTable.cpp
@@ -0,0 +1,46 @@
+#include "SymbolTable.h"
+
+SymbolTable::SymbolTable(){
+	// Symbols
+	m_symbols[";"] = TT_SEMICOLON;
+	m_symbols["="] = TT_ASSIGN;
+
+	// Logic Operators
+	m_symbols["=="] = TT_EQUAL;
+	m_symbols["!"] = TT_NOT_EQUAL;
+	m_symbols["<"] = TT_LOWER;
+	m_symbols["<="] = TT_LOWER_EQUAL;
+	m_symbols[">"] = TT_GREATER;
+	m_symbols[">="] = TT_GREATER_EQUAL;
+
+	// Aritmetic Operators
+	m_symbols["+"] = TT_ADD;
+	m_symbols["-"] = TT_SUB;
+	m_symbols["*"] = TT_MUL;
+	m_symbols["/"] = TT_DIV;
+	m_symbols["%"] = TT_MOD;
+
+	// Keywords
+	m_symbols["program"] = TT_PROGRAM;
+	m_symbols["while"] = TT_WHILE;
+	m_symbols["do"] = TT_DO;
+	m_symbols["done"] = TT_DONE;
+	m_symbols["if"] = TT_IF;
+	m_symbols["then"] = TT_THEN;
+	m_symbols["else"] = TT_ELSE;
+	m_symbols["output"] = TT_OUTPUT;
+	m_symbols["true"] = TT_TRUE;
+	m_symbols["false"] = TT_FALSE;
+	m_symbols["read"] = TT_READ;
+	m_symbols["not"] = TT_NOT;
+
+}
+SymbolTable::~SymbolTable(){
+}
+
+bool SymbolTable::contains(const std::string& token) const {
+	return m_symbols.find(token) != m_symbols.end();
+}
+enum TokenType SymbolTable::find(const std::string token) const {
+	return this->contains(token) ? m_symbols[token] : m_symbols[TT_INVALID_TOKEN];
+}
