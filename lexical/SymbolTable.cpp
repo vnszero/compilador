d0c9f543f276103673594fb00bc4cf960e2d11a3
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-09-23 23:04:30 -0300

    Set Tokens and ST

--- lexical/SymbolTable.cpp
+++ lexical/SymbolTable.cpp
@@ -1,38 +1,46 @@
 #include "SymbolTable.h"
 
 SymbolTable::SymbolTable() {
-	// Symbols
-	m_symbols[";"] = TT_SEMICOLON;
-	m_symbols["="] = TT_ASSIGN;
-
-	// Logic operators
-	m_symbols["=="] = TT_EQUAL;
-	m_symbols["!="] = TT_NOT_EQUAL;
-	m_symbols["<"] = TT_LOWER;
-	m_symbols["<="] = TT_LOWER_EQUAL;
-	m_symbols[">"] = TT_GREATER;
-	m_symbols[">="] = TT_GREATER_EQUAL;
-
 	// Arithmetic operators
-	m_symbols["+"] = TT_ADD;
-	m_symbols["-"] = TT_SUB;
-	m_symbols["*"] = TT_MUL;
-	m_symbols["/"] = TT_DIV;
-	m_symbols["%"] = TT_MOD;
+	m_symbols["+"] = TT_ADD;            // +
+	m_symbols["/"] = TT_DIV;            // /
+	m_symbols["*"] = TT_MUL;            // *
+	m_symbols["-"] = TT_SUB;            // -
 
 	// Keywords
-	m_symbols["program"] = TT_PROGRAM;
-	m_symbols["while"] = TT_WHILE;
-	m_symbols["do"] = TT_DO;
-	m_symbols["done"] = TT_DONE;
-	m_symbols["if"] = TT_IF;
-	m_symbols["then"] = TT_THEN;
-	m_symbols["else"] = TT_ELSE;
-	m_symbols["output"] = TT_OUTPUT;
-	m_symbols["true"] = TT_TRUE;
-	m_symbols["false"] = TT_FALSE;
-	m_symbols["read"] = TT_READ;
-	m_symbols["not"] = TT_NOT;
+	m_symbols["do"] = TT_DO;            // do
+	m_symbols["else"] = TT_ELSE;        // else
+	m_symbols["end"] = TT_END;          // end
+	m_symbols["exit"] = TT_EXIT;		// exit
+	m_symbols["if"] = TT_IF;            // if
+	m_symbols["int"] = TT_INT;			// int
+	m_symbols["float"] = TT_FLOAT;		// float
+	m_symbols["print"] = TT_PRINT;      // print
+	m_symbols["scan"] = TT_SCAN;        // scan
+	m_symbols["start"] = TT_START;      // start
+	m_symbols["string"] = TT_STRING;	// string
+	m_symbols["then"] = TT_THEN;        // then
+	m_symbols["while"] = TT_WHILE;      // while
+
+	// Logic operators
+	m_symbols["&&"] = TT_AND;           // &&
+	m_symbols["!"] = TT_NOT;            // !
+	m_symbols["||"] = TT_OR;         	// ||
+
+	// Relational operator
+	m_symbols["=="] = TT_EQUAL;         // ==
+	m_symbols[">"] = TT_GREATER;        // >
+	m_symbols[">="] = TT_GREATER_EQUAL; // >=
+	m_symbols["<"] = TT_LOWER;          // <
+	m_symbols["<="] = TT_LOWER_EQUAL;   // <=
+	m_symbols["<>"] = TT_NOT_EQUAL;     // <>
+
+	// Symbols
+	m_symbols["="] = TT_ASSIGN;         // =
+	m_symbols[","] = TT_COMMA;		    // ,
+	m_symbols["("] = TT_LEFT_PAR;	    // (
+	m_symbols[")"] = TT_RIGHT_PAR;	    // )
+	m_symbols[";"] = TT_SEMICOLON;      // ;
 }
 
 SymbolTable::~SymbolTable() {
@@ -43,5 +51,5 @@ bool SymbolTable::contains(const std::string& token) const {
 }
 
 enum TokenType SymbolTable::find(const std::string& token) {
-	return this->contains(token) ? m_symbols[token] : TT_VAR;
+	return this->contains(token) ? m_symbols[token] : TT_ID;
 }
--- lexical/TokenType.h
+++ lexical/TokenType.h
@@ -4,53 +4,62 @@
 #include <string>
 
 enum TokenType {
-	// Specials
+	// Alternatives
 	TT_UNEXPECTED_EOF = -2,
 	TT_INVALID_TOKEN  = -1,
 	TT_END_OF_FILE    =  0,
 
-	// Symbols
-	TT_SEMICOLON,     // ;
-	TT_ASSIGN,        // =
-
-	// Logic operators
-	TT_EQUAL,         // ==
-	TT_NOT_EQUAL,     // !=
-	TT_LOWER,         // <
-	TT_LOWER_EQUAL,   // <=
-	TT_GREATER,       // >
-	TT_GREATER_EQUAL, // >=
-
 	// Arithmetic operators
 	TT_ADD,           // +
-	TT_SUB,           // -
-	TT_MUL,           // *
 	TT_DIV,           // /
-	TT_MOD,           // %
+	TT_MUL,           // *
+	TT_SUB,           // -
 
 	// Keywords
-	TT_PROGRAM,       // program
-	TT_WHILE,         // while
 	TT_DO,            // do
-	TT_DONE,          // done
-	TT_IF,            // if
-	TT_THEN,          // then
 	TT_ELSE,          // else
-	TT_OUTPUT,        // output
-	TT_TRUE,          // true
-	TT_FALSE,         // false
-	TT_READ,          // read
-	TT_NOT,           // not
+	TT_END,           // end
+	TT_EXIT,		  // exit
+	TT_IF,            // if
+	TT_INT,			  // int
+	TT_FLOAT,		  // float
+	TT_PRINT,         // print
+	TT_SCAN,          // scan
+	TT_START,         // start
+	TT_STRING,		  // string
+	TT_THEN,          // then
+	TT_WHILE,         // while
 
-	// Others
-	TT_NUMBER,        // number
-	TT_VAR            // variable
+	// Logic operators
+	TT_AND,           // &&
+	TT_NOT,           // !
+	TT_OR,         	  // ||
 
+	// Relational operator
+	TT_EQUAL,         // ==
+	TT_GREATER,       // >
+	TT_GREATER_EQUAL, // >=
+	TT_LOWER,         // <
+	TT_LOWER_EQUAL,   // <=
+	TT_NOT_EQUAL,     // <>
+
+	// Symbols
+	TT_ASSIGN,        // =
+	TT_COMMA,		  // ,
+	TT_LEFT_PAR,	  // (
+	TT_RIGHT_PAR,	  // )
+	TT_SEMICOLON,     // ;
+
+	// Zebras
+	TT_ID,            // identifier
+	TT_INTEGER_CONST, // integer_const
+	TT_FLOAT_CONST,	  // float_const
+	TT_LITERAL		  // literal
 };
 
 inline std::string tt2str(enum TokenType type) {
 	switch (type) {
-		// Specials
+		// Alternatives
 		case TT_UNEXPECTED_EOF:
 			return "UNEXPECTED_EOF";
 		case TT_INVALID_TOKEN:
@@ -58,69 +67,87 @@ inline std::string tt2str(enum TokenType type) {
 		case TT_END_OF_FILE:
 			return "END_OF_FILE";
 
-		// Symbols
-		case TT_SEMICOLON:
-			return "SEMICOLON";
-		case TT_ASSIGN:
-			return "ASSIGN";
+		// Arithmetic operators
+		case TT_ADD:
+			return "ADD";
+		case TT_DIV:
+			return "DIV";
+		case TT_MUL:
+			return "MUL";
+		case TT_SUB:
+			return "SUB";
+
+		// Keywords
+		case TT_DO:
+			return "DO";
+		case TT_ELSE:
+			return "ELSE";
+		case TT_END:
+			return "END";
+		case TT_EXIT:
+			return "EXIT";
+		case TT_IF:
+			return "IF";
+		case TT_INT:
+			return "INT";
+		case TT_FLOAT:
+			return "FLOAT";
+		case TT_PRINT:
+			return "PRINT";
+		case TT_SCAN:
+			return "SCAN";
+		case TT_START:
+			return "START";
+		case TT_STRING:
+			return "STRING";
+		case TT_THEN:
+			return "THEN";
+		case TT_WHILE:
+			return "WHILE";
 
 		// Logic operators
+		case TT_AND:
+			return "AND";
+		case TT_OR:
+			return "OR";
+		case TT_NOT:
+			return "NOT";
+
+		// Relational operators
 		case TT_EQUAL:
 			return "EQUAL";
-		case TT_NOT_EQUAL:
-			return "NOT_EQUAL";
-		case TT_LOWER:
-			return "LOWER";
-		case TT_LOWER_EQUAL:
-			return "LOWER_EQUAL";
 		case TT_GREATER:
 			return "GREATER";
 		case TT_GREATER_EQUAL:
 			return "GREATER_EQUAL";
+		case TT_LOWER:
+			return "LOWER";
+		case TT_LOWER_EQUAL:
+			return "LOWER_EQUAL";
+		case TT_NOT_EQUAL:
+			return "NOT_EQUAL";
 
-		// Arithmetic operators
-		case TT_ADD:
-			return "ADD";
-		case TT_SUB:
-			return "SUB";
-		case TT_MUL:
-			return "MUL";
-		case TT_DIV:
-			return "DIV";
-		case TT_MOD:
-			return "MOD";
+		// Symbols
+		case TT_ASSIGN:
+			return "ASSIGN";
+		case TT_COMMA:
+			return "COMMA";
+		case TT_LEFT_PAR:
+			return "LEFT_PAR";
+		case TT_RIGHT_PAR:
+			return "RIGHT_PAR";
+		case TT_SEMICOLON:
+			return "SEMICOLON";
 
-		// Keywords
-		case TT_PROGRAM:
-			return "PROGRAM";
-		case TT_WHILE:
-			return "WHILE";
-		case TT_DO:
-			return "DO";
-		case TT_DONE:
-			return "DONE";
-		case TT_IF:
-			return "IF";
-		case TT_THEN:
-			return "THEN";
-		case TT_ELSE:
-			return "ELSE";
-		case TT_OUTPUT:
-			return "OUTPUT";
-		case TT_TRUE:
-			return "TRUE";
-		case TT_FALSE:
-			return "FALSE";
-		case TT_READ:
-			return "READ";
-		case TT_NOT:
-			return "NOT";
-
-		// Others
-		case TT_NUMBER:
-			return "NUMBER";
-		case TT_VAR:
-			return "VAR";
+		// Zebras
+		case TT_ID:
+			return "ID";
+		case TT_INTEGER_CONST:
+			return "INTEGER_CONST";
+		case TT_FLOAT_CONST:
+			return "FLOAT_CONST";
+		case TT_LITERAL:
+			return "LITERAL";
 
 		default:
 			throw std::string("invalid token type");
